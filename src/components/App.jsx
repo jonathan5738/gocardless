6e349a57bd8e41f241874f45a7fd463dc44c89bb
jonathan nakahonda <jnakahonda@gmail.com>
2022-11-28 01:19:38 +0100

    added navbar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Landing from '../components/LandingPage/Landing'
+import '../css/variables.css'
+import '../css/general.css'
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-            
+            <Route path='/' element={<Landing/>}/>
         </Routes>
     </BrowserRouter>
   )
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import react from 'react' 
 import ReactDOM from 'react-dom/client' 
-
+import App from './components/App'
 const container = document.querySelector('#root')
 const rooter = ReactDOM.createRoot(container)
+
+rooter.render(<App/>)
