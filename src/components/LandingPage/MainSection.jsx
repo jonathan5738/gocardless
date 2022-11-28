70d27b10ec9a177d9bd3a80c100cb2ec1d014355
jonathan nakahonda <jnakahonda@gmail.com>
2022-11-28 06:43:57 +0100

    added features section

--- src/components/LandingPage/MainSection.jsx
+++ src/components/LandingPage/MainSection.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import '../../css/Landing/MainSection.css'
 import ListCustomers from './ListCustomers'
 import PaymentInfo from './PaymentInfo'
+import FeaturesSection from './FeaturesSection'
 function MainSection() {
   return (
     <section className='main-section'>
         <ListCustomers/>
         <PaymentInfo/>
+        <FeaturesSection/>
     </section>
   )
 }
