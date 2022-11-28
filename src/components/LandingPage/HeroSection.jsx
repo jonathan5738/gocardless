54409d1d86255d2131c5c677151e201c9c2ab9cf
jonathan nakahonda <jnakahonda@gmail.com>
2022-11-28 06:10:16 +0100

    fixed hero-section-text bug

--- src/components/LandingPage/HeroSection.jsx
+++ src/components/LandingPage/HeroSection.jsx
@@ -6,13 +6,15 @@ function HeroSection() {
   return (
     <div className='hero-section'>
       <Navbar/>
-        <div className="hero-section-text">
-             <h1 className='hero-section-text-title'>A NEW ERA IN <br /> PAYMENTS</h1>
-             <p className='hero-section-text-para'>Collect instant, one-off payments. Or automated, recurring payments. Without the chasing, stress, or expensive fees.</p>
-             <div className="hero-section-text-links">
-                 <Link to="#" className='signup'>Sign up</Link>
-                 <Link to="#" className='contact-sales login'>Contact sales</Link>
-             </div>
+        <div className="margin">
+          <div className="hero-section-text">
+              <h1 className='hero-section-text-title'>A NEW ERA IN <br /> PAYMENTS</h1>
+              <p className='hero-section-text-para'>Collect instant, one-off payments. Or automated, recurring payments. Without the chasing, stress, or expensive fees.</p>
+              <div className="hero-section-text-links">
+                  <Link to="#" className='signup'>Sign up</Link>
+                  <Link to="#" className='contact-sales login'>Contact sales</Link>
+              </div>
+          </div>
         </div>
     </div>
   )
